7619e398349dfdf377d9af7a5b5455f6fae61019
eric <cl2324.cs11@nycu.edu.tw>
2024-10-22 22:05:52 +0800

    lab6

--- Lab6/main.py
+++ Lab6/main.py
@@ -12,45 +12,47 @@ def marker_detection(frame, drone):
     return frame, drone
 
 def dodge_marker(drone, id, z):
-        if z>20:
-            drone.send_rc_control(0, 1, 0, 0)
-        elif id == 1:
-            Tello.move(drone, "right", 20)
-        elif id == 2:
-            Tello.move(drone, "left", 20)
+    print(id)
+    if id < 3 and z > 40:
+        Tello.move(drone, "forward", 20)
+        print("C")
+    elif id == 1:
+        Tello.move(drone, "right", 100)
+        print("A")
+        cv2.waitKey(1500)
+    elif id == 2:
+        Tello.move(drone, "left", 100)
+        print("B")
+        cv2.waitKey(1500)
+
 
 def main():
     # Tello
     drone = Tello()
     drone.connect()
-    #time.sleep(10)
+    battery = drone.get_battery()
+    print(f"Battery: {battery}%")
+    time.sleep(5)
     drone.streamon()
     frame_read = drone.get_frame_read()
     
     while True:
         frame = frame_read.frame
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        
-        dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-
+        dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
         parameters = cv2.aruco.DetectorParameters_create()
-
         markerCorners, markerIDs, rejectedCandidates = cv2.aruco.detectMarkers(frame, dictionary, parameters=parameters)
-        print(markerIDs)
-
+        # print(markerIDs)
         frame = cv2.aruco.drawDetectedMarkers(frame, markerCorners, markerIDs)
-
         fs = cv2.FileStorage("1.xml", cv2.FILE_STORAGE_READ)
         intrinsic = fs.getNode("instrinsic").mat()
         distorsion = fs.getNode("distortion").mat()
-        
         rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(markerCorners, 15, intrinsic, distorsion)
         # print(rvecs, tvecs)
-        if drone.is_flying:
-            x, y, z = tvecs[0][0]
-            frame = cv2.aruco.drawAxis(frame, intrinsic, distorsion, rvecs, tvecs, 10)
-            frame = cv2.putText(frame, f"x: {x:.2f}, y: {y:.2f}, z: {z:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        
+        if rvecs is not None and tvecs is not None:
             for i in range(rvecs.shape[0]):
+                x, y, z = tvecs[0][0]
                 id = markerIDs[i][0]
                 if id is not None:
                     dodge_marker(drone, id, z)
