da815e8a6e079b1368eaf43e2a1ad36ec3ae5a20
eric <cl2324.cs11@nycu.edu.tw>
2024-12-06 17:22:50 +0800

    final add dolldetect

--- final/main.py
+++ final/main.py
@@ -5,6 +5,9 @@ import math
 from djitellopy import Tello
 from pyimagesearch.pid import PID
 from keyboard_djitellopy import keyboard
+from face_detection import face_detection
+from dectectDoll import detectDoll
+
 
 def level1(drone, frame_read):
     # 起飛，看人臉往上往前往下，看人臉往下往前往上
@@ -21,7 +24,7 @@ def level1(drone, frame_read):
         
         # todo:數字須調整
         frame, cx, cy, dis = face_detection(frame, 30, f_y)
-        if (state == 0 || state == 2):
+        if (state == 0 or state == 2):
             if (cx > 0): Tello.move(drone, "left", 20)
             elif (cx < 0): Tello.move(drone, "right", 20)
             elif (cy > 0): Tello.move(drone, "up", 20)
@@ -43,6 +46,11 @@ def level1(drone, frame_read):
 def level2(drone, frame_read):
     # 識別娃娃，回傳決定追線二不同階段
     # todo:放入lab8的東東
+    while(1):
+        frame = frame_read.frame
+        doll = detectDoll(frame) #1 Carno, 2 Melody, 0 no detect
+        if(doll != 0):
+            return doll; 
     return 0
 
 def trace_prior_up(drone, frame_read, break_cond, init_angle):
@@ -55,20 +63,26 @@ def trace_prior_left(drone, frame_read, break_cond, init_angle):
     
 def trace_until_aruco(drone, frame_read):
     # 往左追線，看到marker時轉180
-        
+    dummy = 0
 
 def trace(drone, frame_read, prior):
     # 往上，追線優先往上/左，追線往下鑽過table，追線優先往左/上，往左走看到marker，轉180 結束
-    if(prior == 0):
+    if(prior == 1):
         trace_prior_up(drone, frame_read, 5, 0)
-        trace_prior_left(drone, frame_read, 4, 270) # 中間prior不重要
-        trace_prior_left(drone, frame_read, 10, 270)
+        trace_prior_left(drone, frame_read, 4, 270)
+        trace_until_aruco(drone, frame_read)
+    elif(prior == 2):
+        trace_prior_left(drone, frame_read, 2, 0)
+        trace_prior_up(drone, frame_read, 7, 180)
+        trace_until_aruco(drone, frame_read)
+    else:
+        print("unknown prior")
     
     
 def level3(drone, frame_read, leftOrRight):
     # 寫死往左前方或右前方後降落
-    if(leftOrRight == 0): Tello.move(drone, "left", 50)
-    if(leftOrRight == 1): Tello.move(drone, "right", 50)
+    if(leftOrRight == 1): Tello.move(drone, "left", 50)
+    if(leftOrRight == 2): Tello.move(drone, "right", 50)
     Tello.move(drone, "forward", 180)
 
 
